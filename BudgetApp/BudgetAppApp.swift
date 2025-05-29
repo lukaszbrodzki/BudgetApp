3ab07d645c1e0792a6b5a2273b986d4bdbb872be
Łukasz Brodzki <lukaszbrodzki@gmail.com>
2025-05-29 21:14:36 +0200

    Initial commit

---
+++ BudgetApp/BudgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetAppApp.swift
+//  BudgetApp
+//
+//  Created by Łukasz Brodzki on 29/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct BudgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
